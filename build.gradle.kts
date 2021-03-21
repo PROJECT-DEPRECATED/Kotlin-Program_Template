50b381061f8639773f6d00bac0f23b0c70a14719
Project_TL <kimpiao020@gmail.com>
2021-03-22 06:27:51 +0900

    This is base

---
+++ build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    kotlin("jvm") version "1.4.31"
+    id("com.github.johnrengelman.shadow") version "6.1.0"
+    `maven-publish`
+}
+
+group = properties["programGroup"]!!
+version = properties["programVersion"]!!
+
+repositories {
+    mavenCentral()
+
+    jcenter()
+    maven("https://jitpack.io/")
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
+
+tasks {
+    javadoc {
+        options.encoding = "UTF-8"
+    }
+
+    compileKotlin {
+        kotlinOptions.jvmTarget = "11"
+    }
+
+    create<Jar>("sourceJar") {
+        archiveClassifier.set("source")
+        sourceSets["main"].allSource
+    }
+
+    jar {
+        manifest {
+            attributes["Main-Class"] = project.properties["programMain"]!!
+        }
+    }
+
+    shadowJar {
+        archiveBaseName.set(project.name)
+        archiveClassifier.set("")
+        archiveVersion.set("")
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>(project.name) {
+            artifact(tasks["sourceJar"])
+            from(components["java"])
+        }
+    }
+}
---
+++ src/main/kotlin/org/projecttl/program/example/ExampleProgram.kt
@@ -0,0 +1,11 @@
+package org.projecttl.program.example
+
+class ExampleProgram {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<out String>) {
+            println("Hello, World!")
+            // TODO Write code here!
+        }
+    }
+}
